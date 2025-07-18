2969f2e7338e38a53b7b983eab7fefe095ca1741
Aliny Kelly <alinykellyfs@gmail.com>
2025-07-18 10:21:16 -0300

    alteracoes

--- src/botoes/BtnPreencherConcluido.java
+++ src/botoes/BtnPreencherConcluido.java
@@ -1,2 +1,27 @@
-package botoes;public class BtnPreencherConcluido {
+package botoes;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+
+import java.math.BigDecimal;
+
+public class BtnPreencherConcluido implements AcaoRotinaJava {
+
+    @Override
+    public void doAction(ContextoAcao contextoAcao) throws Exception {
+
+        Registro[] linhas = contextoAcao.getLinhas();
+
+        for (Registro linha : linhas) {
+
+
+            BigDecimal codEmp = (BigDecimal) linha.getCampo("CODEMP");
+
+            linha.setCampo("CONCLUIDO", "S");
+            linha.save();
+
+        }
+    }
+
 }
--- src/eventos/EvtAtualizarCampo.java
+++ src/eventos/EvtAtualizarCampo.java
@@ -1,4 +1,83 @@
 package eventos;
 
-public class EvtAtualizarData {
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.vo.EntityVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.MGEModelException;
+import br.com.sankhya.modelcore.auth.AuthenticationInfo;
+import br.com.sankhya.modelcore.util.DynamicEntityNames;
+import br.com.sankhya.ws.ServiceContext;
+
+import java.math.BigDecimal;
+
+public class EvtAtualizarCampo implements EventoProgramavelJava {
+
+    @Override
+    public void beforeInsert(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void beforeUpdate(PersistenceEvent persistenceEvent) throws Exception {
+
+        System.out.println("Evento techday");
+        BigDecimal usuarioLogado = ((AuthenticationInfo) ServiceContext.getCurrent().getAutentication()).getUserID();
+
+        DynamicVO oldValue = (DynamicVO) persistenceEvent.getOldVO();
+        String descOld = oldValue.asString("DESCRICAO");
+
+        DynamicVO newValue = (DynamicVO) persistenceEvent.getVo();
+        String descNew = newValue.asString("DESCRICAO");
+
+        if (!descNew.equals(descOld)) {
+
+
+            JapeSession.SessionHandle hnd = null;
+            try {
+                hnd = JapeSession.open();
+
+                JapeWrapper usuarioDAO = JapeFactory.dao(DynamicEntityNames.USUARIO);
+                DynamicVO usuario = usuarioDAO.findByPK(usuarioLogado);
+                String nome = usuario.asString("NOMEUSU");
+                newValue.setProperty("LOG", "O usuario cód.: " + usuarioLogado + " nome: " + nome + " alterou o campo, de " + descOld + " para " + descNew);
+
+            } catch (Exception e) {
+                MGEModelException.throwMe(e);
+            } finally {
+                JapeSession.close(hnd);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void beforeDelete(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterInsert(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterUpdate(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void afterDelete(PersistenceEvent persistenceEvent) throws Exception {
+
+    }
+
+    @Override
+    public void beforeCommit(TransactionContext transactionContext) throws Exception {
+
+    }
 }
